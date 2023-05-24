e6acaae8e350f6e9c883b7ea9c2bf840164fd80e
Joseph <jturnism@gmail.com>
2023-05-24 17:31:42 -0600

    output cleanup attempt

--- maintenance.py
+++ maintenance.py
@@ -9,27 +9,25 @@ def announce_script():
     print("│┼────────────────────────────────────────────────┼│")
     print("││                   Maintenance                  ││")
     print("││                                                ││")
-    print("││ver. 230524B                                 -JT││")
+    print("││ver. 230524D                                 -JT││")
     print("│┼────────────────────────────────────────────────┼│")
     print("└──────────────────────────────────────────────────┘")
 
 def run_command(command, description):
     print("\n")
-    print(f"┌──────────────────────────────────────────────────┼")
+    print("┌──────────────────────────────────────────────────┼")
     print(f"| {description}")
-    print(f"|                                                  ┼")
+    print("|                                                  ┼")
     try:
         completed_process = subprocess.run(command, capture_output=True, text=True, timeout=600, shell=True, check=True)
-        print(f"| {completed_process.stdout.strip()}{' '*(50-len(description)-len(completed_process.stdout.strip()))}(DONE) |")
+        print("|                                           (DONE) |")
         print("└──────────────────────────────────────────────────┘")
     except subprocess.CalledProcessError as e:
-        print(f"| {e.stderr.strip()}{' '*(50-len(description)-len(e.stderr.strip()))}(ERROR) |")
+        print("|                                          (ERROR) |")
         print("└──────────────────────────────────────────────────┘")
 
 def main():
-    print("Script started.")
     announce_script()
-
     os_version = platform.system()
     if os_version == "Windows":
         release_version = platform.release()
@@ -37,8 +35,8 @@ def main():
             run_command("dism /Online /Cleanup-image /Restorehealth", "Running: Deployment Image Servicing and Management")
             run_command("sfc /scannow", "Running: System File Checker")
             run_command("net stop wuauserv", "Stopping: Windows Update Service")
-            run_command('del /s /q "C:/Windows/Temp"', "Deleting: C:\Windows\Temp")
-            run_command('del /s /q "C:/Windows/SoftwareDistribution"', "Deleting: C:\Windows\SoftwareDistribution")
+            run_command('del /s /q "C:/Windows/Temp"', "Cleaning: C:\Windows\Temp")
+            run_command('del /s /q "C:/Windows/SoftwareDistribution"', "Cleaning: C:\Windows\SoftwareDistribution")
             run_command("sc config wuauserv start=auto", "Configuring: Windows Update Service")
             run_command("net start wuauserv", "Starting: Windows Update Service")
             run_command("USOCLIENT.EXE RefreshSettings StartScan StartDownload ScanInstallWait StartInstall", "Installing: Updates")
@@ -50,11 +48,10 @@ def main():
         print("┌──────────────────────────────────────────────────┐")
         print("│Unsupported Operating System              (ERROR) │")
         print("└──────────────────────────────────────────────────┘")
-
+    print("\n")
     print("┌──────────────────────────────────────────────────┐")
     print("│                    Complete                      │")
     print("└──────────────────────────────────────────────────┘")
-    print("Script finished.")
     sleep(14400)
 
 if __name__ == '__main__':
